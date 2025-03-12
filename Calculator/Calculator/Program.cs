8b507709f5abf5937745af85464f15a05d7b3293
danubagoes <rifkydanuasmoro@students.amikom.ac.id>
2025-03-13 05:07:03 +0700

    Percobaan kedua

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 6;
+            Console.WriteLine($"Hasil penambahan : {a} + {b} = {a + b}");
+            Console.WriteLine($"Hasil Pengurangan : {a} + {b} = {a + b}");
+
         }
     }
 }
